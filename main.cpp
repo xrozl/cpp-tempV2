3c714fea0c91398ffc5c045bd8b972e75dc26e3e
yuu <92621736+xrozl@users.noreply.github.com>
2022-05-30 10:35:45 +0900

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define int long long
+
+#define pb push_back
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+
+#define fastio ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+
+using namespace std;
+
+int32_t main(void) {
+    fastio;
+    
+    // write code here
+    
+    
+}
